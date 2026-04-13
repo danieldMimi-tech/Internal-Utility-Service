55e08e70aa9d598ad9936e7842ae980b50a3d7eb
mirabel Daniel <danieldmirabel@gmail.com>
2026-04-13 15:51:18 -0700

    Add Dockerfile, nginx config and update app.py

--- app.py
+++ app.py
@@ -4,17 +4,24 @@ import config
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return jsonify({
         "message": "Internal Utility Service Running",
-        "environment": config.ENVIRONMENT,
-        "db_host": config.DB_HOST  #leaking config info
+        "environment": config.ENVIRONMENT
     })
+
 
 @app.route("/users")
 def users():
     return jsonify(get_users())
 
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "UP"}), 200
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)  #debug mode ON
+    app.run(host="0.0.0.0", port=5000, debug=False)
